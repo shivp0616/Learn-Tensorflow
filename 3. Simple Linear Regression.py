99a283f60609499ef3a0e3f75de507a314da1291
Shiv Prakash <30807615+shivp0616@users.noreply.github.com>
2019-02-11 17:57:09 +0530

    Update 3. Simple Linear Regression.py

--- 3. Simple Linear Regression.py
+++ 3. Simple Linear Regression.py
@@ -1,17 +1,15 @@
 import numpy as np 
 import tensorflow as tf 
 import matplotlib.pyplot as plt
-np.random.seed(101) 
-tf.set_random_seed(101)
+import pandas as pd
 
-# Genrating random linear data 
-# There will be 50 data points ranging from 0 to 50 
-x = np.linspace(0, 50, 50) 
-y = np.linspace(0, 50, 50) 
-  
-# Adding noise to the random linear data 
-x += np.random.uniform(-4, 4, 50) 
-y += np.random.uniform(-4, 4, 50) 
+
+#importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+
+x = dataset.iloc[:,0].values
+y = dataset.iloc[:,1].values
+
   
 n = len(x) # Number of data points
 
